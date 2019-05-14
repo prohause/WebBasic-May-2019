2f8beb038c993bfe335e2c68d2a889157f8a99cb
Gyunay Daud <prohause@abv.bg>
2019-05-14 19:58:05 +0300

    Initial

---
+++ SoftUniHTTPHelper/SoftUniHTTPHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftUniHTTPHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
