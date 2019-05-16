e7d0d801f00949529e23a1e935cc8a5c08b4612e
Gyunay Daud <prohause@abv.bg>
2019-05-16 11:34:57 +0300

    SIS initial commit

---
+++ SIS/Demo.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using Demo.App.Controllers;
+using SIS.HTTP.Enums;
+using SIS.WebServer;
+using SIS.WebServer.Result;
+using SIS.WebServer.Routing;
+using SIS.WebServer.Routing.Contracts;
+
+namespace Demo.App
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Add(HttpRequestMethod.Get, "/", httpRequest
+                => new HomeController().Home(httpRequest));
+
+            Server server = new Server(80, serverRoutingTable);
+            server.Run();
+        }
+    }
+}
--- SoftUniHTTPHelper/SoftUniHTTPHelper/Program.cs
+++ SoftUniHTTPHelper/SoftUniHTTPHelper/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace SoftUniHTTPHelper
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tcpListener = new TcpListener(IPAddress.Loopback, 14444);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+
+                using (var stream = client.GetStream())
+                {
+                    var requestBytes = new byte[1000000];
+                    var readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
+                    var result = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
+
+                    Console.WriteLine(new string('=', 80));
+                    Console.WriteLine(result);
+
+                    var sb = new StringBuilder();
+                    var responseBody = "<form><input type='text' name='tweet' placeholder='Enter tweet...' /><input type='Submit' /></form>";
+                    sb.AppendLine("HTTP/1.0 200 OK");
+                    sb.AppendLine("Server: MyCustomServer/1.0");
+                    sb.AppendLine($"Content-Length: {responseBody.Length}");
+                    sb.AppendLine("Content-Type: text/html");
+                    sb.AppendLine();
+                    sb.AppendLine(responseBody);
+                    var response = Encoding.UTF8.GetBytes(sb.ToString());
+                    stream.Write(response, 0, response.Length);
+                }
+            }
         }
     }
 }
