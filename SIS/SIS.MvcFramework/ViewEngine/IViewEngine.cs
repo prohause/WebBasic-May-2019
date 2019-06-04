6c1e9b83b782b38bdb6c9e69b2ed5a7345d596ee
prohause@abv.bg <Sevdjan8092>
2019-06-04 16:10:09 +0300

    Commit

--- SIS/SIS.MvcFramework/ViewEngine/IViewEngine.cs
+++
@@ -1,9 +0,0 @@
-﻿using SIS.MvcFramework.Identity;
-
-namespace SIS.MvcFramework.ViewEngine
-{
-    public interface IViewEngine
-    {
-        string GetHtml<T>(string viewContent, T model, Principal user);
-    }
-}
