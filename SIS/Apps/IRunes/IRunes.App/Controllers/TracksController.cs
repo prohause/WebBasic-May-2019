ec0d49e282b9003529b7b454cf4d8ffea55f9768
prohause@abv.bg <Sevdjan8092>
2019-06-04 16:11:25 +0300

    agagin

---
+++ SIS/Apps/IRunes/IRunes.App/Controllers/TracksController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IRunes.App.ViewModels;
+using IRunes.Models;
+using IRunes.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Mapping;
+using SIS.MvcFramework.Result;
+
+namespace IRunes.App.Controllers
+{
+    public class TracksController : Controller
+    {
+        private readonly ITrackService trackService;
+
+        private readonly IAlbumService albumService;
+
+        public TracksController()
+        {
+            this.trackService = new TrackService();
+            this.albumService = new AlbumService();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            string albumId = this.Request.QueryData["albumId"].ToString();
+
+            return this.View(new TrackCreateViewModel{ AlbumId = albumId });
+        }
+
+        [Authorize]
+        [HttpPost(ActionName = "Create")]
+        public ActionResult CreateConfirm()
+        {
+            string albumId = this.Request.QueryData["albumId"].ToString();
+            string name = ((ISet<string>)this.Request.FormData["name"]).FirstOrDefault();
+            string link = ((ISet<string>)this.Request.FormData["link"]).FirstOrDefault();
+            string price = ((ISet<string>)this.Request.FormData["price"]).FirstOrDefault();
+
+            Track trackForDb = new Track
+            {
+                Name = name,
+                Link = link,
+                Price = decimal.Parse(price)
+            };
+
+            if (!this.albumService.AddTrackToAlbum(albumId, trackForDb))
+            {
+                return this.Redirect("/Albums/All");
+            }
+
+            return this.Redirect($"/Albums/Details?id={albumId}");
+        }
+
+        [Authorize]
+        public ActionResult Details()
+        {
+            string albumId = this.Request.QueryData["albumId"].ToString();
+            string trackId = this.Request.QueryData["trackId"].ToString();
+
+            Track trackFromDb = this.trackService.GetTrackById(trackId);
+
+            if (trackFromDb == null)
+            {
+                return this.Redirect($"/Albums/Details?id={albumId}");
+            }
+
+            TrackDetailsViewModel trackDetailsViewModel = ModelMapper.ProjectTo<TrackDetailsViewModel>(trackFromDb);
+            trackDetailsViewModel.AlbumId = albumId;
+
+            return this.View(trackDetailsViewModel);
+        }
+    }
+}
