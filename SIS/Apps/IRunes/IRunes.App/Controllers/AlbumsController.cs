aa3cc0440693e857537c52df1b459207b92f671a
prohause@abv.bg <Sevdjan8092>
2019-06-05 19:57:12 +0300

    Commit

---
+++ SIS/Apps/IRunes/IRunes.App/Controllers/AlbumsController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IRunes.App.ViewModels;
+using IRunes.Models;
+using IRunes.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Mapping;
+using SIS.MvcFramework.Result;
+
+namespace IRunes.App.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private readonly IAlbumService albumService;
+
+        public AlbumsController(IAlbumService albumService)
+        {
+            // new is glue
+            this.albumService = albumService;
+        }
+
+        [Authorize]
+        public ActionResult All()
+        {
+            ICollection<Album> allAlbums = this.albumService.GetAllAlbums();
+
+            if (allAlbums.Count != 0)
+            {
+                return this.View(allAlbums.Select(ModelMapper.ProjectTo<AlbumAllViewModel>).ToList());
+            }
+
+            return this.View(new List<AlbumAllViewModel>());
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(string name, string cover)
+        {
+            Album album = new Album
+            {
+                Name = name,
+                Cover = cover,
+                Price = 0M
+            };
+
+            this.albumService.CreateAlbum(album);
+
+            return this.Redirect("/Albums/All");
+        }
+
+        [Authorize]
+        public ActionResult Details(string id)
+        {
+            Album albumFromDb = this.albumService.GetAlbumById(id);
+
+            AlbumDetailsViewModel albumViewModel = ModelMapper.ProjectTo<AlbumDetailsViewModel>(albumFromDb);
+
+            if (albumFromDb == null)
+            {
+                return this.Redirect("/Albums/All");
+            }
+
+            return this.View(albumViewModel);
+        }
+    }
+}
