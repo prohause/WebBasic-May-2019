497c4a61e6cced5e7ce629af65bca8a20f428404
Gyunay Daud <prohause@abv.bg>
2019-06-06 06:54:27 +0300

    commit

---
+++ SIS/Apps/IRunes/IRunes.App/Controllers/TracksController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IRunes.App.ViewModels;
+using IRunes.App.ViewModels.Tracks;
+using IRunes.Models;
+using IRunes.Services;
+using Microsoft.Extensions.Logging;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Security;
+using SIS.MvcFramework.Mapping;
+using SIS.MvcFramework.Result;
+
+namespace IRunes.App.Controllers
+{
+    public class TracksController : Controller
+    {
+        private readonly ITrackService trackService;
+
+        private readonly IAlbumService albumService;
+
+        public TracksController(ITrackService trackService, IAlbumService albumService)
+        {
+            this.trackService = trackService;
+            this.albumService = albumService;
+        }
+
+        [Authorize]
+        public ActionResult Create(string albumId)
+        {
+            return this.View(new TrackCreateViewModel{ AlbumId = albumId });
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CreateInputModel model)
+        {
+            Track trackForDb = new Track
+            {
+                Name = model.Name,
+                Link = model.Link,
+                Price = model.Price,
+            };
+
+            if (!this.albumService.AddTrackToAlbum(model.AlbumId, trackForDb))
+            {
+                return this.Redirect("/Albums/All");
+            }
+
+            return this.Redirect($"/Albums/Details?id={model.AlbumId}");
+        }
+
+        [Authorize]
+        public ActionResult Details(string albumId, string trackId)
+        {
+            Track trackFromDb = this.trackService.GetTrackById(trackId);
+
+            if (trackFromDb == null)
+            {
+                return this.Redirect($"/Albums/Details?id={albumId}");
+            }
+
+            TrackDetailsViewModel trackDetailsViewModel = ModelMapper.ProjectTo<TrackDetailsViewModel>(trackFromDb);
+            trackDetailsViewModel.AlbumId = albumId;
+
+            return this.View(trackDetailsViewModel);
+        }
+    }
+}
---
+++ SIS/Apps/IRunes/IRunes.App/Controllers/UsersController.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using IRunes.Models;
+using IRunes.Services;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes;
+using SIS.MvcFramework.Attributes.Action;
+using SIS.MvcFramework.Result;
+
+namespace IRunes.App.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [NonAction]
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                return Encoding.UTF8.GetString(sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            User userFromDb = this.userService.GetUserByUsernameAndPassword(username, this.HashPassword(password));
+
+            if (userFromDb == null)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            this.SignIn(userFromDb.Id, userFromDb.Username, userFromDb.Email);
+
+            return this.Redirect("/");
+        }
+
+        public ActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string username, string password, string confirmPassword, string email)
+        {
+            if (password != confirmPassword)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            User user = new User
+            {
+                Username = username,
+                Password = this.HashPassword(password),
+                Email = email
+            };
+
+            this.userService.CreateUser(user);
+
+            return this.Redirect("/Users/Login");
+        }
+
+        public ActionResult Logout()
+        {
+            this.SignOut();
+
+            return this.Redirect("/");
+        }
+    }
+}
